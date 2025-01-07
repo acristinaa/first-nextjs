326fdf960cf0232e2c089b572940d7b23a5368e7
Cristina Ayala <crisstinaayala777@gmail.com>
2025-01-07 14:33:10 +0800

    pages

---
+++ app/about/team/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function Page() {
+  return (
+    <Link href='/about'>
+      <div>
+        <h1>Cristina</h1>
+      </div>
+    </Link>
+  );
+}
+
+export default Page;
